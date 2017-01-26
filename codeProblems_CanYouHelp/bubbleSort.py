5e390c0c42e6695d36ef0adfa0865d79d555b999
Dominic Carrington <carringtondominic@gmail.com>
2017-01-26 14:59:24 +0000

    Update bubbleSort.py
    Fixed version of code.
    - list.insert parameters were inversed
    - list.remove uses the value of what to remove, not the index
    
    Python Documentation:
    https://docs.python.org/3/tutorial/datastructures.html

--- codeProblems_CanYouHelp/bubbleSort.py
+++ codeProblems_CanYouHelp/bubbleSort.py
@@ -7,23 +7,32 @@ print("list = " + str(list1))
 
 #list1length = len(list1) - 1
 #for l in range (0, list1length):
-for y in range (0, 36):
+for y in range (0, len(list1) - 1 * len(list1) - 1): # Looping too few times
     x = i + 1
 #    print("i = " + str(i))
 #    print("x = " + str(x))
 #    print("list = " + str(list1))
+    print("----------")
     print("i = {0}: value = {1}".format(i,list1[i] ))
     print("x = {0}: value = {1}".format(x,list1[x] ))
     print(list1)
 
     
     if list1[i] > list1[i + 1]: 
-        tempi = list1[i]
-        tempx = list1[x]
-        list1.remove(x)
-        list1.remove(i)
-        list1.insert(tempi, x)
-        list1.insert(tempx, i)
+        tempi = list1[i] ###
+        tempx = list1[x] ###
+
+        # https://docs.python.org/3/tutorial/datastructures.html
+        
+        #list1.remove(x)
+        list1.remove(tempx) # Required value to remove, not index
+        #list1.remove(i)
+        list1.remove(tempi) # Required value to remove, not index
+        
+        #list1.insert(tempi, x)
+        list1.insert(x, tempi) # Index, value (not value, index)
+        #list1.insert(tempx, i)
+        list1.insert(i, tempx) # Index, value (not value, index)
 
     if i == 8:
         i = 0
@@ -31,3 +40,4 @@ for y in range (0, 36):
         i += 1
 	
 print("list = " + str(list1))
+
